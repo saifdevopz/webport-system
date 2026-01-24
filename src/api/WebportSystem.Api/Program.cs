495be1bf54aad832f55c4ec58cc09c62f9ff50fe
saifd <saifdevopz@gmail.com>
2026-01-24 10:39:45 +0200

    gg

--- src/api/WebportSystem.Api/Program.cs
+++ src/api/WebportSystem.Api/Program.cs
@@ -111,7 +111,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.MapOpenApi();
     app.MapScalarApiReference("/", _ =>
     {
-        _.WithTitle("My API Documentation");
+        _.WithTitle("My API Documentationzzz");
         _.Servers = [];
         _.Theme = ScalarTheme.Kepler;
     });
